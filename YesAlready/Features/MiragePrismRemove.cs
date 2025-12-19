e03634ab2aa2587acc94af46b4c0352878df341d
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2025-12-19 23:13:28 +0000

    fix prism remove

--- YesAlready/Features/MiragePrismRemove.cs
+++ YesAlready/Features/MiragePrismRemove.cs
@@ -6,4 +6,18 @@ internal class MiragePrismRemove : AddonFeature
     protected override bool IsEnabled() => C.MiragePrismRemoveDispel;
 
     protected override unsafe void HandleAddonEvent(AddonEvent eventType, AddonArgs addonInfo, AtkUnitBase* atk) => new AddonMaster.MiragePrismRemove(atk).Dispel();
+
+    public partial class AddonMaster
+    {
+        public class MiragePrismRemove : AddonMasterBase<AtkUnitBase>
+        {
+            public unsafe AtkComponentButton* DispelButton => Addon->GetComponentButtonById(14);
+            public override string AddonDescription { get; } = "Remove glamour window";
+
+            public MiragePrismRemove(nint addon) : base(addon) { }
+            public unsafe MiragePrismRemove(void* addon) : base(addon) { }
+
+            public unsafe void Dispel() => ClickButtonIfEnabled(DispelButton);
+        }
+    }
 }
